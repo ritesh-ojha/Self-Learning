3650acc80314274125b63729f3b34dca116f6c66
ritesh-ojha <reeteshojha5@gmail.com>
2024-03-14 12:28:41 +0530

    Day 2 Working with Files and Directory with 20+ commands

--- Shell_Scripting/commands.sh
+++ Shell_Scripting/commands.sh
@@ -4,7 +4,7 @@ source logo.sh
 
 
 
-echo "Day 1"
+echo "			Day 1"
 echo ""
 
 echo "echo ""			- Use to print into terminal"
@@ -12,3 +12,27 @@ echo "mkdir <file>		- Use to create directory"
 echo "touch <file>		- Use to create file"
 echo "mv <old> <new>		- Use to rename file"
 echo "$var			- Use to get variable value"
+echo ""
+
+echo "			Day 2"
+echo ""
+
+echo "help			- Use for Terminal help"
+echo "--help			- Use to get help for particular command [ ls --help ]"
+echo "ls			- Use for listing files and directory"
+echo "ls -a			- Use to see all files and directory"
+echo "ls -l			- Use to see detailes of list of directory"
+echo "ls -h			- Use to see the size of of listing files"
+echo "ls -alh			- Combine command for three task into one"
+echo "pwd			- Print working directory"
+echo "cd /repo/			- change working directory"
+echo "cd 			- Goto Home directory"
+echo "cd -			- Goto previous directory"
+echo "cp [s] [d]		- Copy file"
+echo "cp [old] [new]		- Copy file with new name"
+echo "cp -r [s] [d]		- Copy full directory	[ -r recursive ]"
+echo "mv [s] [d]		- Move file"
+echo "rm			- Remove file"
+echo "rm -r [dir]		- Remove full directory"
+
+
