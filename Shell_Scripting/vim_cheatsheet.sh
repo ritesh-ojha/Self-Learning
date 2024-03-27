baccf3636626bf65656cbb94db29bb191037758a
ritesh-ojha <reeteshojha5@gmail.com>
2024-03-27 19:53:08 +0530

    Vim editor commands cheatsheet

---
+++ Shell_Scripting/vim_cheatsheet.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+#
+#############
+#Author Ritesh
+#Date	23-03-2024
+#
+#############
+
+echo "vim <file_name>		Open file in Vim editor"
+echo "i				Insert text before the cursor"
+echo "a				Append text after the cursor"
+echo "o				Open a new line below the current line and enter insert mode."
+echo "shift + o			Open a new line above the current line and enter insert mode"
+echo "x				Delete the character under the cursor"
+echo "dd			Delete the current line"
+echo "yy			Yank (copy) the current line"
+echo "p				Paste the copied or deleted text after the cursor"
+echo "u				Undo the last change"
+echo "ctrl + r			Redo the last change"
+echo "/<word>			Search forward for a pattern"
+echo "?<word>			Search backward for a pattern"
+echo "n				Move to the next occurrence of the search pattern"
+echo "shift + N			Move to the previous occurrence of the search pattern"
+echo ":s/<word>/<replace>	Replace the first occurrence of a pattern in the current line"
+echo ":s/pattern/replacement/g	Replace all occurrences of a pattern in the current line"
+echo ":%s/pattern/replacement/g	Replace all occurrences of a pattern in the entire file"
+echo "gg			Move to the beginning of the file"
+echo "shift + g			Move to the end of the file"
+echo ":line_number		Move to a specific line number"
+echo ":w			Save the current file"
+echo ":wq or :x			save and quit vim"
+echo ":q			Quit Vim"
+echo ":q!			Quit without saving"
+
+
+
+
+
+
